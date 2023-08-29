1419be39218d4e3d17a9a4e30c6d07f209d59b61
ThiaudioTT <thiago.sky25@gmail.com>
2023-08-29 17:10:33 -0300

    feat: adds some testing

--- cypress/e2e/testando.cy.ts
+++ cypress/e2e/testando.cy.ts
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Página de cadastro', () => {
+  it('Preencher os campos do formulario corretamente para novo cadastro', () => {
+    cy.visit('http://localhost:4200/#/home');
+    cy.contains('a', 'Register now').click();
+    // cy.get('[formcontrolname="email"]').type('catarina@email.com.br');
+    // cy.get('[formcontrolname="fullName"]').type('Catarina Pessoa');
+    // cy.get('[formcontrolname="userName"]').type('catarinap');
+    // cy.get('[formcontrolname="password"]').type('catarina123');
+
+    // cy.contains('button', 'Register').click();
+
+    cy.get('[data-test="email"]').type('catarina@email.com.br');
+    cy.get('[data-test="fullName"]').type('Catarina Pessoa');
+    cy.get('[data-test="registerUserName"]').type('catarinap');
+    cy.get('[data-test="registerPassword"]').type('catarina123');
+    cy.contains('button', 'Register').click();
+
   })
 })
