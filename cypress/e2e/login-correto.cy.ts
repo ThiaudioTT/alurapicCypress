ee8f961e5f64c1de3a506b7add04347c9edd9d9c
ThiaudioTT <thiago.sky25@gmail.com>
2023-08-31 16:29:24 -0300

    feat: add mais alguns testes

---
+++ cypress/e2e/login-correto.cy.ts
@@ -0,0 +1,19 @@
+describe('Página de login', () => {
+  it('Verificar mensagem de campos obrigatórios', () => {
+    cy.visit('http://localhost:4200/#/home');
+
+    // login: catarinap
+    // senha: catarina123
+
+    cy.contains('User name is required!').should('be.visible');
+    cy.contains('Password is required!').should('be.visible');
+
+    cy.get('[data-test="loginUserName"]').type('catarinap');
+    // cy.contains('User name is required!').should('not.be.visible');
+
+    cy.get('[data-test="loginPassword"]').type('catarina123');
+    // cy.contains('Password is required!').should('not.be.visible');
+
+    cy.contains('[data-test="loginBtn"]', 'login').click();
+  });
+});
